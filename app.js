afbc7b6af495989538e5f166359c62916e3ca9df
Asia <avroraka2004@gmail.com>
2025-06-24 14:17:53 +0300

    template for the app

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+const indexRouter = require("./routes/indexRouter");
+
+app.use("/", indexRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Mini Message Board  - listening on port ${PORT}! `);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
